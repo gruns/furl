2cf970b862569b536400fd3c36a3125737d2a05b
Arthur Grunseid <rleeroy@gmail.com>
2011-12-14 00:33:43 -0500

    setup.py for PyPI.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='furl',
+      version='0.1.30',
+      packages=find_packages(),
+      author='Arthur Grunseid',
+      author_email='grunseid@gmail.com',
+      url='https://github.com/gruns/furl',
+      license='Build Amazing Things (Unlicense)',
+      include_package_data=True,
+      description='URL manipulation made simple.',
+      long_description='URL parsing and manipulation made simple.',
+      platforms=['any'],
+      classifiers='',
+      install_requires=[''],
+      test_suite='tests',
+      tests_require=[],
+      )
