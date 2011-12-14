cc6a56babea97f031ac515e3c7e5f664a0fffb76
Arthur Grunseid <rleeroy@gmail.com>
2011-12-14 17:18:10 -0500

    Version 0.2.1 for PyPI and Python 2.6.

--- furl/__init__.py
+++ furl/__init__.py
@@ -10,7 +10,7 @@
 # License: Build Amazing Things (Unlicense)
 
 __title__ = 'furl'
-__version__ = '0.2'
+__version__ = '0.2.1'
 __author__ = 'Arthur Grunseid'
 __license__ = 'Unlicense'
 __copyright__ = 'LOLOLOL'
