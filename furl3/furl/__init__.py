901df257355d6bcfff8a3c244d39efac44b4c044
Omoto Kenji <doloopwhile@gmail.com>
2011-12-26 03:28:22 +0900

    2to3

---
+++ furl3/furl/__init__.py
@@ -0,0 +1,18 @@
+#
+# furl: URL manipulation made simple.
+#
+# Arthur Grunseid
+# grunseid.com
+# grunseid@gmail.com
+#
+# Copyright: LOLOLOL
+#
+# License: Build Amazing Things (Unlicense)
+
+__title__ = 'furl'
+__version__ = '0.2.2'
+__author__ = 'Arthur Grunseid'
+__license__ = 'Unlicense'
+__copyright__ = 'LOLOLOL'
+
+from .furl import *
