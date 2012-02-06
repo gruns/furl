2069a2d2ffde70adef04dc932f9d077e90b6b996
Arthur Grunseid <rleeroy@gmail.com>
2012-02-06 08:01:35 -0500

    Version 0.3 for PyPi with fixed setup.py.

--- furl/__init__.py
+++ furl/__init__.py
@@ -7,6 +7,11 @@
 #
 # License: Build Amazing Things (Unlicense)
 
-from __info__ import *
+__title__ = 'furl'
+__version__ = '0.3' # Keep synchronized with setup.py.
+__author__ = 'Arthur Grunseid'
+__contact__ = 'grunseid@gmail.com'
+__license__ = 'Unlicense'
+__url__ = 'https://github.com/gruns/furl'
 
 from .furl import *
--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 import sys
 from setuptools import setup, find_packages
 
-import __info__ as pkginfo
-
 if sys.argv[-1] == 'publish':
   os.system('python setup.py sdist upload')
   sys.exit()
@@ -11,16 +9,16 @@ long_description = ('''furl: URL parsing and manipulation made simple.
 
 Information and documentation at: https://github.com/gruns/furl''')
 
-setup(name=pkginfo.__title__,
-      version=pkginfo.__version__,
-      author=pkginfo.__author__,
-      author_email=pkginfo.__contact__,
-      url=pkginfo.__url__,
-      packages=find_packages(),
-      license=pkginfo.__license__,
-      include_package_data=True,
+setup(name='furl',
+      version='0.3', # Keep synchronized with __init__.py.
+      author='Arthur Grunseid',
+      author_email='grunseid@gmail.com',
+      url='https://github.com/gruns/furl',
+      license='Unlicense',
       description='URL manipulation made simple.',
       long_description=long_description,
+      packages=find_packages(),
+      include_package_data=True,
       platforms=['any'],
       classifiers=['Topic :: Internet',
                    'Natural Language :: English',
