df99bd34f284e05d5cb1a8c8b28bc60d44e6a06a
Arthur Grunseid <rleeroy@gmail.com>
2015-04-01 16:41:21 -0700

    Furl v0.4.5

--- furl/__init__.py
+++ furl/__init__.py
@@ -8,7 +8,7 @@
 # License: Build Amazing Things (Unlicense)
 
 __title__ = 'furl'
-__version__ = '0.4.4'
+__version__ = '0.4.5'
 __license__ = 'Unlicense'
 __author__ = 'Arthur Grunseid'
 __contact__ = 'grunseid@gmail.com'
