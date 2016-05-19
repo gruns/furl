84a98bfa3c619988fed387fac021d291a36bfe78
Arthur Grunseid <grunseid@gmail.com>
2016-05-19 12:49:35 -0700

    Furl v0.4.95. Bump due to PyPi 500 Internal Server Error.

--- furl/__init__.py
+++ furl/__init__.py
@@ -10,7 +10,7 @@
 # License: Build Amazing Things (Unlicense)
 
 __title__ = 'furl'
-__version__ = '0.4.94'
+__version__ = '0.4.95'
 __license__ = 'Unlicense'
 __author__ = 'Arthur Grunseid'
 __contact__ = 'grunseid@gmail.com'
