64c38aea3bbb89d4483ac790ef9a59712e8b9c09
Arthur Grunseid <grunseid@gmail.com>
2016-11-15 04:35:55 -0800

    Run 'setup.py test' and 'setup.py publish' through setuptools.setup()'s cmdclass parameter. By default, 'setup.py test' requires test/__init__.py to exist, which isn't discorouged.

--- setup.py
+++ setup.py
@@ -14,17 +14,52 @@ import os
 import re
 import sys
 from os.path import dirname, join as pjoin
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+from setuptools.command.test import test as TestCommand
+
 
 with open(pjoin(dirname(__file__), 'furl', '__init__.py')) as fd:
     VERSION = re.compile(
         r".*__version__ = '(.*?)'", re.S).match(fd.read()).group(1)
 
-if sys.argv[-1] == 'publish':
+
+class Publish(Command):
     """Publish to PyPI with twine."""
-    os.system('python setup.py sdist')
-    os.system('twine upload dist/furl-%s.tar.gz' % VERSION)
-    sys.exit()
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('python setup.py sdist')
+        rc = os.system('twine upload dist/furl-%s.tar.gz' % VERSION)
+        sys.exit(rc)
+
+
+class RunTests(TestCommand):
+    """
+    Run the unit tests.
+
+    By default, `python setup.py test` fails if tests/ isn't a Python
+    module (that is, if the tests/ directory doesn't contain an
+    __init__.py file). But the tests/ directory shouldn't contain an
+    __init__.py file and tests/ shouldn't be a Python module. See
+
+      http://doc.pytest.org/en/latest/goodpractices.html
+
+    Running the unit tests manually here enables `python setup.py test`
+    without tests/ being a Python module.
+    """
+    def run_tests(self):
+        from unittest import TestLoader, TextTestRunner
+        tests_dir = pjoin(dirname(__file__), 'tests')
+        suite = TestLoader().discover(tests_dir)
+        result = TextTestRunner().run(suite)
+        sys.exit(0 if result.wasSuccessful() else -1)
+
 
 long_description = (
     'Information and documentation at https://github.com/gruns/furl.')
@@ -63,6 +98,9 @@ setup(
         'six>=1.8.0',
         'orderedmultidict>=0.7.8',
     ],
-    test_suite='tests',
+    cmdclass={
+        'test': RunTests,
+        'publish': Publish,
+    },
     tests_require=tests_require,
 )
