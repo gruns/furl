c6033984493cea6f701589c3c6ee4a5e9892d17a
Ansgar Grunseid <grunseid@gmail.com>
2018-08-23 18:17:49 -0700

    Ignore lambda expression usage in IceCream import fallback.

--- furl/furl.py
+++ furl/furl.py
@@ -21,7 +21,7 @@ from six.moves.urllib.parse import quote, unquote
 try:
     from icecream import ic
 except ImportError:  # Graceful fallback if IceCream isn't installed.
-    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)
+    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
 
 from .omdict1D import omdict1D
 from .compat import string_types, UnicodeMixin
