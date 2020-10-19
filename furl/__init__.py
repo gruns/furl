4cb2ea1e106b6018a5dc32b71816698d47ebd011
ThatXliner <66848002+ThatXliner@users.noreply.github.com>
2020-10-18 17:02:00 -0700

    Update __init__.py

--- furl/__init__.py
+++ furl/__init__.py
@@ -11,14 +11,6 @@
 #
 
 from .furl import *  # noqa
-from .__version__ import (  # noqa
-    __title__, __version__, __license__, __author__, __contact__, __url__,
-    __description__)
+from .__version__ import *
 
-__all__ = ["furl"]
 
-# Alternative
-# from . import __version__ as v, furl
-# __version__ = v.__version__
-# __title__ = v.__title__
-# etc.
