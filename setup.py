87d451f7893edc50128dfa9c13e77a7f261acb41
Ansgar Grunseid <grunseid@gmail.com>
2021-04-16 11:19:15 -0700

    declare furl compatible with py3.8 and py3.9 in setup.py

--- setup.py
+++ setup.py
@@ -92,6 +92,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
